01be45b52726d8ad4ea357e3479eeef2eb8936b8
Andres <andres.gomez.rodriguez2003@gmail.com>
2025-04-23 01:20:25 -0400

    agregue pagina de inicio y agregue su ruta

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tiendita PM</title>
+</head>
+<body>
+    Estructura de la pagina
+</body>
+</html>
