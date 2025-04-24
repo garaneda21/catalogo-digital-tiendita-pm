48bc8464a21d6a7458a491a95c2cada01b2dede6
Gabo <gabo.alucema@gmail.com>
2025-04-24 00:07:03 -0400

    creado archivo de migracion de tabla administrador

---
+++ database/migrations/2025_04_24_032819_create_administrador_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrador', function (Blueprint $table) {
+            $table->id('id_admin')->unsigned();
+            $table->string('nombre_admin', 50);
+            $table->string('correo_admin', 100);
+            $table->string('pwd', 255);
+            $table->boolean('activo')->default(0);
+            $table->primary('id_admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrador');
+    }
+};
