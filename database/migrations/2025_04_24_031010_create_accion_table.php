c93b5560bca51d005dce3ae83180f5ce4d7c0a28
Gabo <gabo.alucema@gmail.com>
2025-04-23 23:45:05 -0400

    pequeña modificacion

--- database/migrations/2025_04_24_031010_create_accion_table.php
+++ database/migrations/2025_04_24_031010_create_accion_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAccionTable extends Migration
+return new class extends Migration
 {
     /**
      * Ejecutar las migraciones.
@@ -14,9 +14,9 @@ class CreateAccionTable extends Migration
     public function up()
     {
         Schema::create('accion', function (Blueprint $table) {
-            $table->id('id_accion')->unsigned();
-            $table->integer('nombre_accion')->unsigned();
-            $table->primary('id_accion');
+            $table->id('id_accion')->unsigned();  // Define un ID auto-incremental y sin signo
+            $table->integer('nombre_accion')->unsigned();  // Define un entero sin signo
+            $table->primary('id_accion');  // Define la clave primaria
         });
     }
 
@@ -29,4 +29,4 @@ class CreateAccionTable extends Migration
     {
         Schema::dropIfExists('accion');
     }
-}
+};
