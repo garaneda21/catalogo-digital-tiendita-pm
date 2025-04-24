f420e0f1b4d32ae95e72fe199dbc6f1a491eb99e
Gabo <gabo.alucema@gmail.com>
2025-04-24 00:09:11 -0400

    creado archivo de migracion de tabla producto

---
+++ database/migrations/2025_04_24_035824_create_producto_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id('id_producto')->unsigned();
+            $table->string('nombre_producto', 250);
+            $table->text('descripcion')->nullable();
+            $table->integer('stock_actual')->unsigned()->default(0);
+            $table->decimal('precio', 12, 2)->default(0.00);
+            $table->string('imagen_url', 500)->nullable();
+            $table->foreignId('id_estado')->constrained('estado')->onDelete('cascade');
+            $table->foreignId('id_categoria')->constrained('categoria')->onDelete('cascade');
+            $table->primary('id_producto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producto');
+    }
+};
