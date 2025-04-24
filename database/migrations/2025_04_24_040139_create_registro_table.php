95c5ca12a6e7735a959e817e7198d84ef200bb7c
Gabo <gabo.alucema@gmail.com>
2025-04-24 00:10:14 -0400

    creado archivo de migracion de tabla registro

---
+++ database/migrations/2025_04_24_040139_create_registro_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registro', function (Blueprint $table) {
+            $table->id('id_registro')->unsigned();
+            $table->dateTime('fecha_hora');
+            $table->foreignId('id_accion')->constrained('accion')->onDelete('cascade');
+            $table->foreignId('id_producto')->constrained('producto')->onDelete('cascade');
+            $table->foreignId('id_admin')->constrained('administrador')->onDelete('cascade');
+            $table->primary('id_registro');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registro');
+    }
+};
