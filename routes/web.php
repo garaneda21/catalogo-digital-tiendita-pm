f6c692f17b3dad927c653feccae8595031adc7bb
Gabo <gabo.alucema@gmail.com>
2025-05-03 00:21:58 -0400

    agregada ruta de prueba para panel de admin

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ use App\Http\Controllers\CategoriaController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/panel', function () {
+    $vista = request()->get('vista');
+    return view('admin.panel', compact('vista'));
+})->name('panel');
+*/
