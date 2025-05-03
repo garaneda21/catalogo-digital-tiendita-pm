644669be6b38385c053c5203512f61a60072bc27
Gabo <gabo.alucema@gmail.com>
2025-05-03 00:21:10 -0400

    eliminadas rutas de auth para el login

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,3 @@ use App\Http\Controllers\CategoriaController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/admin/login', [AdminController::class, 'showLoginForm'])->name('admin.login');
-Route::post('/admin/login', [AdminController::class, 'login'])->name('admin.login.submit');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-    Route::post('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
-
-    // Rutas para productos
-    Route::resource('admin/productos', ProductoController::class);
-
-    // Rutas para categorías
-    Route::resource('admin/categorias', CategoriaController::class);
-});
