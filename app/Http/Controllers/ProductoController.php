7ee418933be34e7369198a5821fe174f2a203659
garaneda21 <garaneda@utem.cl>
2025-05-10 00:57:01 -0400

    mejorado formulario usando "templates" y terminado soporte de imagen

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Categoria;
 use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ProductoController extends Controller
 {
@@ -13,14 +14,13 @@ class ProductoController extends Controller
         $query = Producto::with('categoria');
 
         if ($request->has('search')) {
-            $query->where('nombre_producto', 'like', '%' . $request->search . '%');
+            $query->where('nombre_producto', 'like', '%'.$request->search.'%');
         }
 
         $productos = $query->paginate(10);
 
         return view('admin.productos.index', compact('productos'));
     }
-
 
     public function create()
     {
@@ -42,13 +42,13 @@ class ProductoController extends Controller
             'descripcion' => [],
             'stock_actual' => ['gte:0', 'max:9'],
             'precio' => ['required', 'gte:0', 'max:9'],
-            'image' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'imagen' => ['image', 'mimes:jpeg,png,jpg', 'max:2048'],
         ]);
 
         // para la imágen
-        if ($request->image) {
-            $url_imagen = '/images/' . time() . '.' . $request->image->extension();
-            $request->image->move(public_path('images'), $url_imagen);
+        if ($request->imagen) {
+            $url_imagen = '/images/productos/'.time().'.'.$request->imagen->extension();
+            $request->imagen->move(public_path('images/productos'), $url_imagen);
         }
 
         Producto::create([
@@ -77,6 +77,7 @@ class ProductoController extends Controller
     public function edit(Producto $producto)
     {
         $categorias = Categoria::all();
+
         return view('admin.productos.edit', compact('producto', 'categorias'));
     }
 
@@ -91,7 +92,19 @@ class ProductoController extends Controller
             'descripcion' => [],
             'stock_actual' => ['nullable', 'gte:0', 'max:9'],
             'precio' => ['required', 'gte:0', 'max:9'],
+            'imagen' => ['image', 'mimes:jpeg,png,jpg', 'max:2048'],
         ]);
+
+        // para la imágen
+        if ($request->imagen) {
+            $url_imagen = '/images/productos/'.time().'.'.$request->imagen->extension();
+            $request->imagen->move(public_path('images/productos'), $url_imagen);
+
+            // TODO: eliminar imagen anterior
+            /* if ($producto->imagen_url && File::exists($producto->imagen_url)) { */
+            /*     File::delete($producto->imagen_url); */
+            /* } */
+        }
 
         $producto->update([
             'nombre_producto' => $request->input('nombre_producto'),
@@ -99,6 +112,7 @@ class ProductoController extends Controller
             'descripcion' => $request->input('descripcion'),
             'stock_actual' => $request->input('stock_actual'),
             'precio' => $request->input('precio'),
+            'imagen_url' => $url_imagen ?? $producto->imagen_url,
         ]);
 
         return redirect()->route('productos.index')->with('success', 'Producto actualizado con éxito');
--- resources/views/admin/productos/create.blade.php
+++ resources/views/admin/productos/create.blade.php
@@ -5,123 +5,83 @@
 
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base/7 font-semibold text-gray-900">Nuevo Producto</h2>
+                <h2 class="text-base font-semibold text-gray-900">Nuevo Producto</h2>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
                     <!-- Nombre Producto -->
-                    <div class="col-span-full">
-                        <label for="nombre_producto" class="block text-sm/6 font-medium text-gray-900">Nombre
-                            Producto <span class="text-sm text-gray-500">(requerido)</span></label>
+                    <x-form-field>
+                        <x-form-label for="nombre_producto">Nombre Producto <span
+                                class="text-blue-500">(requerido)</span></x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="text" name="nombre_producto" id="nombre_producto"
-                                    class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6">
-                            </div>
+                            <x-form-input type="text" name="nombre_producto" id="nombre_producto"
+                                :value="old('nombre_producto')"></x-form-input>
                         </div>
-                    </div>
+                        <x-form-error name="nombre_producto"></x-form-error>
+                    </x-form-field>
 
                     <!-- Categoría -->
-                    <div class="col-span-full">
-                        <label for="categoria" class="block text-sm/6 font-medium text-gray-900">Categoría <span
-                                class="text-sm text-gray-500">(requerido)</span></label>
-                        <div class="mt-2 grid grid-cols-1">
-
-                            <select id="categoria" name="categoria"
-                                class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-
-                                @foreach ($categorias->all() as $categoria)
-                                <option value="{{ $categoria->id }}">{{ $categoria->nombre_categoria }}</option>
-                                @endforeach
-
-                            </select>
-
-                            <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                                viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd"
-                                    d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </div>
-                    </div>
+                    <x-form-field>
+                        <x-form-label for="categoria">Categoría <span
+                                class="text-blue-500">(requerido)</span></x-form-label>
+                        <x-form-select>
+                            @foreach ($categorias->all() as $categoria)
+                                <option value="{{ $categoria->id }}"
+                                    {{ old('categoria') == $categoria->id ? 'selected' : '' }}>
+                                    {{ $categoria->nombre_categoria }}</option>
+                            @endforeach
+                        </x-form-select>
+                    </x-form-field>
 
                     <!-- Precio -->
-                    <div class="col-span-full">
-                        <label for="precio" class="block text-sm/6 font-medium text-gray-900">Precio <span
-                                class="text-sm text-gray-500">(requerido)</span></label>
+                    <x-form-field>
+                        <x-form-label for="precio">Precio <span class="text-blue-500">(requerido)</span></x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="text" name="precio" id="precio"
-                                    class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6">
-                            </div>
+                            <x-form-input type="text" name="precio" id="precio" :value="old('precio')"></x-form-input>
                         </div>
-                    </div>
+                        <x-form-error name="precio"></x-form-error>
+                    </x-form-field>
 
                     <!-- Stock Actual -->
-                    <div class="col-span-full">
-                        <label for="stock_actual" class="block text-sm/6 font-medium text-gray-900">Stock
-                            Actual</label>
+                    <x-form-field>
+                        <x-form-label for="stock_actual">Stock Actual</x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="number" name="stock_actual" id="stock_actual" min="0"
-                                    class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6">
-                            </div>
+                            <x-form-input type="text" name="stock_actual" id="stock_actual" :value="old('stock_actual')"
+                                min="0"></x-form-input>
                         </div>
-                    </div>
+                        <x-form-error name="stock_actual"></x-form-error>
+                    </x-form-field>
 
                     <!-- Descripción -->
-                    <div class="col-span-full">
-                        <label for="descripcion" class="block text-sm/6 font-medium text-gray-900">Descripción</label>
+                    <x-form-field>
+                        <x-form-label for="descripcion">Descripción</x-form-label>
                         <div class="mt-2">
-                            <textarea name="descripcion" id="descripcion" rows="3"
-                                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                            <x-form-textarea type="text" name="descripcion" id="descripcion">
+                                {{ old('descripcion') }} </x-form-textarea>
                         </div>
-                    </div>
+                    </x-form-field>
 
                     <!-- Imagen -->
-                    <div class="col-span-full">
-                        <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover photo</label>
-                        <div
-                            class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                            <div class="text-center">
-                                <svg class="mx-auto size-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor"
-                                    aria-hidden="true" data-slot="icon">
-                                    <path fill-rule="evenodd"
-                                        d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                <div class="mt-4 flex text-sm/6 text-gray-600">
-                                    <label for="image"
-                                        class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500">
-                                        <span>Upload a file</span>
-                                        <input id="image" name="image" type="file" class="sr-only">
-                                    </label>
-                                    <p class="pl-1">or drag and drop</p>
-                                </div>
-                                <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                            </div>
-                        </div>
-                    </div>
+                    <x-form-field>
+                        <x-form-label for="imagen">Imagen del producto</x-form-label>
+
+                        <div class="mt-2">
+                            <x-form-input-image></x-form-input-image>
+                        </div </x-form-field>
 
 
-                    @if ($errors->any())
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                    @endif
+                        @if ($errors->any())
+                            <x-form-errorcard></x-form-errorcard>
+                        @endif
                 </div>
             </div>
-        </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <!-- <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancelar</button> -->
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Guardar</button>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <!-- <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancelar</button> -->
+                <button type="submit"
+                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Guardar</button>
+            </div>
         </div>
     </form>
 
