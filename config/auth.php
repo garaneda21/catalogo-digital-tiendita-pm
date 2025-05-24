634c796de18a5ec2cf8836379b7cf0ee4d599d68
Gabo <gabo.alucema@gmail.com>
2025-05-23 21:44:48 -0400

    creado guard y provider personalizado

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'administradores',
+        ],
     ],
 
     /*
@@ -63,6 +68,11 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
+        ],
+
+        'administradores'=> [
+            'driver' => 'eloquent',
+            'model' => App/Models/Administrador::class,
         ],
 
         // 'users' => [
