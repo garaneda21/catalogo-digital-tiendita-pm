e23ac740569e3e0ed2b5461676f2dfe50e707180
Gabo <gabo.alucema@gmail.com>
2025-05-23 21:37:21 -0400

    modelo Administrador actualizado para login con auth

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -2,20 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
-class Administrador extends Model
+class Administrador extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\AdministradorFactory> */
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $table = 'administradores';
 
     protected $fillable = [
         'nombre_admin',
         'correo_admin',
-        'password', // evaluar si es seguro
+        'password', 
         'activo',
+    ];
+
+    protected $hidden = [
+        'password',
     ];
 }
