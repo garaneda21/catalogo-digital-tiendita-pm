46f2b9b7544325ead716ba927edd1260da539aff
garaneda21 <garaneda@utem.cl>
2025-05-30 00:20:24 -0400

    se creo el método de busqueda para la clase Producto
    esto permite que todos los controladoes (o clases que hereden de
    producto), puedan realizar esta operación

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -23,4 +23,36 @@ class Producto extends Model
     {
         return $this->belongsTo(Categoria::class);
     }
+
+    // Método de búsqueda y ordenamiento
+    static public function busqueda($request, $query) {
+        if ($request->has('search')) {
+            $query->where('nombre_producto', 'like', '%'.$request->search.'%');
+        }
+
+        // Ordenamiento
+        switch ($request->ordering) {
+            case 'recientes':
+                $query->orderBy('created_at', 'desc');
+                break;
+            case 'nombre_asc':
+                $query->orderBy('nombre_producto', 'asc');
+                break;
+            case 'nombre_desc':
+                $query->orderBy('nombre_producto', 'desc');
+                break;
+            case 'precio_asc':
+                $query->orderBy('precio', 'asc');
+                break;
+            case 'precio_desc':
+                $query->orderBy('precio', 'desc');
+                break;
+            default:
+                $query->orderBy('created_at', 'desc');
+                break;
+        }
+
+        // Paginación con parámetros persistentes
+        return $query->paginate(10)->appends($request->only(['search', 'ordering']));
+    }
 }
