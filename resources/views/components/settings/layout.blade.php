d750fa0f784b00f1c3d4f65b055a0656c0185a98
garaneda21 <garaneda@utem.cl>
2025-05-31 15:16:22 -0400

    se elimino wire:navigate que evitaba que cargara el dropdown de usuario

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -1,9 +1,9 @@
 <div class="flex items-start max-md:flex-col">
     <div class="me-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
-            <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Perfil') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.password')" wire:navigate>{{ __('Contraseña') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('Apariencia') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.profile')">{{ __('Perfil') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.password')">{{ __('Contraseña') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.appearance')">{{ __('Apariencia') }}</flux:navlist.item>
         </flux:navlist>
     </div>
 
