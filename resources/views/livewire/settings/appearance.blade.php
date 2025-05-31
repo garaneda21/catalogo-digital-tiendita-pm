fd0ecc8771491631d89446c2bb6a3be45fcbf2ff
Andres <andres.gomez.rodriguez2003@gmail.com>
2025-05-30 21:21:40 -0400

    Se adapto el layout de settings desde componentes de settings

--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -1,11 +1,15 @@
-<section class="w-full">
-    @include('partials.settings-heading')
-
-    <x-settings.layout :heading="__('Apariencia')" :subheading=" __('Actualiza la apariencia para tu cuenta')">
-        <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
-            <flux:radio value="light" icon="sun">{{ __('Claro') }}</flux:radio>
-            <flux:radio value="dark" icon="moon">{{ __('Oscuro') }}</flux:radio>
-            <flux:radio value="system" icon="computer-desktop">{{ __('Sistema') }}</flux:radio>
-        </flux:radio.group>
-    </x-settings.layout>
-</section>
+<flux:main>
+    <div class="bg-white dark:bg-azul-oscuro shadow-xl rounded-2xl p-8 border dark:border-gray-700 min-h-full">        
+        <section class="w-full">
+            @include('partials.settings-heading')
+        
+            <x-settings.layout :heading="__('Apariencia')" :subheading=" __('Actualiza la apariencia para tu cuenta')">
+                <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
+                    <flux:radio value="light" icon="sun">{{ __('Claro') }}</flux:radio>
+                    <flux:radio value="dark" icon="moon">{{ __('Oscuro') }}</flux:radio>
+                    <flux:radio value="system" icon="computer-desktop">{{ __('Sistema') }}</flux:radio>
+                </flux:radio.group>
+            </x-settings.layout>
+        </section>
+    </div>
+</flux:main>
--- resources/views/livewire/settings/profile.blade.php
+++ resources/views/livewire/settings/profile.blade.php
@@ -1,43 +1,47 @@
-<section class="w-full">
-    @include('partials.settings-heading')
-
-    <x-settings.layout :heading="__('Perfil')" :subheading="__('Actualiza tu nombre y dirección de correo electrónico')">
-        <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
-            <flux:input wire:model="name" :label="__('Nombre')" type="text" required autofocus autocomplete="name" />
-
-            <div>
-                <flux:input wire:model="email" :label="__('Email')" type="email" required autocomplete="email" />
-
-                @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&! auth()->user()->hasVerifiedEmail())
+<flux:main>
+    <div class="bg-white dark:bg-azul-oscuro shadow-xl rounded-2xl p-8 border dark:border-gray-700 min-h-full">
+        <section class="w-full">
+            @include('partials.settings-heading')
+        
+            <x-settings.layout :heading="__('Perfil')" :subheading="__('Actualiza tu nombre y dirección de correo electrónico')">
+                <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
+                    <flux:input wire:model="name" :label="__('Nombre')" type="text" required autofocus autocomplete="name" />
+                
                     <div>
-                        <flux:text class="mt-4">
-                            {{ __('Tu correo no está verificado.') }}
-
-                            <flux:link class="text-sm cursor-pointer" wire:click.prevent="resendVerificationNotification">
-                                {{ __('Clickea aquí para re-enviar email de verificación.') }}
-                            </flux:link>
-                        </flux:text>
-
-                        @if (session('status') === 'verification-link-sent')
-                            <flux:text class="mt-2 font-medium !dark:text-green-400 !text-green-600">
-                                {{ __('Un nuevo link de verificación ha sido enviado a tu correo electrónico.') }}
-                            </flux:text>
+                        <flux:input wire:model="email" :label="__('Email')" type="email" required autocomplete="email" />
+                    
+                        @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&! auth()->user()->hasVerifiedEmail())
+                            <div>
+                                <flux:text class="mt-4">
+                                    {{ __('Tu correo no está verificado.') }}
+                            
+                                    <flux:link class="text-sm cursor-pointer" wire:click.prevent="resendVerificationNotification">
+                                        {{ __('Clickea aquí para re-enviar email de verificación.') }}
+                                    </flux:link>
+                                </flux:text>
+                            
+                                @if (session('status') === 'verification-link-sent')
+                                    <flux:text class="mt-2 font-medium !dark:text-green-400 !text-green-600">
+                                        {{ __('Un nuevo link de verificación ha sido enviado a tu correo electrónico.') }}
+                                    </flux:text>
+                                @endif
+                            </div>
                         @endif
                     </div>
-                @endif
-            </div>
-
-            <div class="flex items-center gap-4">
-                <div class="flex items-center justify-end">
-                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Guardar cambios') }}</flux:button>
-                </div>
-
-                <x-action-message class="me-3" on="profile-updated">
-                    {{ __('Se han guardado los cambios.') }}
-                </x-action-message>
-            </div>
-        </form>
-
-        <livewire:settings.delete-user-form />
-    </x-settings.layout>
-</section>
+                
+                    <div class="flex items-center gap-4">
+                        <div class="flex items-center justify-end">
+                            <flux:button variant="primary" type="submit" class="w-full">{{ __('Guardar cambios') }}</flux:button>
+                        </div>
+                    
+                        <x-action-message class="me-3" on="profile-updated">
+                            {{ __('Se han guardado los cambios.') }}
+                        </x-action-message>
+                    </div>
+                </form>
+            
+                <livewire:settings.delete-user-form />
+            </x-settings.layout>
+        </section>
+    </div>
+</flux:main>
