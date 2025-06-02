0e9e956b34359424b63400cdf603b57759a56fd1
garaneda21 <garaneda@utem.cl>
2025-06-02 00:48:37 -0400

    limpieza de componetes

--- resources/views/components/forms/input-imagen.blade.php
+++ resources/views/components/forms/input-imagen.blade.php
@@ -1,16 +1,14 @@
 @props(['imagen_actual' => null])
 
-<input type="file" id="imagen" name="imagen" accept="image/*" onchange="previewDeImagen(event)"
-    class="block w-full rounded-md bg-white text-sm text-gray-700 border file:mr-4 file:py-2 file:px-4 file:border-0 file:text-sm file:font-semibold file:bg-indigo-600 file:text-white hover:file:bg-indigo-500">
+<flux:input type="file" id="imagen" name="imagen" accept="image/*" label="Imagen del Producto" onchange="previewDeImagen(event)" />
 
 @if ($imagen_actual)
-    <img id="preview" src="{{ asset($imagen_actual) }}" alt="Vista previa"
-        class="mt-2 w-40 h-40 object-cover border rounded-lg">
+   <img id="preview" src="{{ asset($imagen_actual) }}" alt="Vista previa"
+        class="w-40 h-40 object-cover border rounded-lg">
 @else
     <img id="preview" src="{{ asset('images/placeholder.png') }}" alt="Vista previa"
-        class="mt-2 w-40 h-40 object-cover border rounded-lg hidden">
+        class="w-40 h-40 object-cover border rounded-lg hidden">
 @endif
-
 
 <script>
     function previewDeImagen(event) {
