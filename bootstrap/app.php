08207f75df8e52493adecffe59b65036821db504
garaneda21 <garaneda@utem.cl>
2025-06-14 17:19:26 -0400

    redirijir a '/' si un usuario logeado quierer acceder a rutas de admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {})
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectUsersTo('/');
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->renderable(function (AuthenticationException $e) {
             $guard = $e->guards()[0] ?? 'web';
