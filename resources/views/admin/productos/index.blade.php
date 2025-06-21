1fa6ff4b8f651345a6e0ff4aa326e427ab485e5c
garaneda21 <garaneda@utem.cl>
2025-06-20 21:59:36 -0400

    actualizado módulo de productos para mostrar más información

--- resources/views/admin/productos/index.blade.php
+++ resources/views/admin/productos/index.blade.php
@@ -27,126 +27,67 @@
 
             <x-ordenamiento-y-busqueda></x-ordenamiento-y-busqueda>
 
-            @foreach ($productos as $producto)
-                <div
-                    class="bg-white border-2 rounded-xl p-3 flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-6">
+            <div class="mb-4">
+                {{ $productos->links() }}
+            </div>
 
-                    <!-- Imagen -->
-                    <div class="w-full md:w-20 h-20 bg-gray-100 rounded-lg overflow-hidden flex-shrink-0">
-                        <img src="{{ $producto->imagen_url ? asset('storage/' . $producto->imagen_url) : '/images/placeholder-product.jpg' }}"
-                            alt="{{ $producto->nombre_producto }}" class="w-full h-full object-cover">
-                    </div>
+            <div class="overflow-auto rounded-lg border">
+                <table class="min-w-full border-separate border-spacing-0 text-sm">
+                    <thead class="bg-gray-200 text-azul-profundo">
+                        <tr>
+                            <th class="w-24 px-4 py-2 text-left">Imágen</th>
+                            <th class="px-4 py-2 text-left">Nombre Producto</th>
+                            <th class="w-50 px-4 py-2 text-left">Categoría</th>
+                            <th class="w-34 px-4 py-2 text-left">Precio</th>
+                            <th class="w-16 px-4 py-2 text-left">Stock</th>
+                            <th class="w-34 px-4 py-2 text-left">Estado Stock</th>
+                            <th class="w-60 px-4 py-2 text-right">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white text-[#3D3C63]">
+                        @foreach ($productos as $producto)
+                            <tr class="odd:bg-white even:bg-gray-100"">
+                                <td class="px-4 py-2">
+                                    <div class="w-15 h-15 bg-gray-100 rounded-lg overflow-hidden flex-shrink-0">
+                                        <img src="{{ $producto->imagen_url ? asset('storage/' . $producto->imagen_url) : '/images/placeholder-product.jpg' }}"
+                                            alt="{{ $producto->nombre_producto }}" class="w-full h-full object-cover">
+                                    </div>
+                                </td>
+                                <td class="px-4 py-2 whitespace-nowrap font-bold">{{ $producto->nombre_producto }}</td>
+                                <td class="px-4 py-2 whitespace-nowrap">{{ $producto->categoria->nombre_categoria }}</td>
+                                <td class="px-4 py-2 whitespace-nowrap">{{ '$' . number_format($producto->precio, 0, ',', '.') }}</td>
+                                <td class="px-4 py-2 whitespace-nowrap font-bold">{{ $producto->stock_actual }}</td>
+                                <td class="px-4 py-2 whitespace-nowrap">
+                                    @if ($producto->stock_actual >= 6)
+                                        <flux:badge variant="pill" color="zinc">Normal</flux:badge>
+                                    @elseif ($producto->stock_actual > 0)
+                                        <flux:badge variant="pill" color="yellow">Bajo</flux:badge>
+                                    @else
+                                        <flux:badge variant="pill" color="red">Agotado</flux:badge>
+                                    @endif
+                                </td>
+                                <td class="space-x-2 px-4 py-2 whitespace-nowrap font-bold text-right ">
+                                    <flux:button href="/admin/movimientos/entrada/{{ $producto->id }}/create-stock"
+                                        icon="plus-circle" class="text-amber-700!" tooltip="Ingresar Stock" />
 
-                    <!-- Info del producto -->
-                    <div class="flex-1 space-y-1">
-                        <h2 class="text-lg font-semibold text-gray-800"><a href="#" data-bs-toggle="modal"
-                                data-bs-target="#modal{{ $producto->id }}">{{ $producto->nombre_producto }}</a></h2>
+                                    <flux:button href="/admin/movimientos/salida/{{ $producto->id }}/create-venta"
+                                        icon="banknotes" class="text-green-700!" tooltip="Venta rápida" />
 
-                        <div class="text-sm text-gray-600">
-                            <span class="font-medium">Precio:</span> ${{ $producto->precio }}<br>
-                            <span class="font-medium">Stock:</span> {{ $producto->stock_actual }} unidades<br>
-                            <span class="font-medium">Categoría:</span>
-                            {{ $producto->categoria->nombre_categoria ?? 'SIN CATEGORÍA' }}
-                        </div>
-                    </div>
+                                    @can('view', App\Models\Producto::class)
+                                        <flux:button href="{{ route('productos.edit', $producto->id) }}" icon="list-bullet"
+                                            class="text-blue-700!" tooltip="Ver detalles" />
+                                    @endcan
 
-
-                    <flux:button href="/admin/movimientos/entrada/{{ $producto->id }}/create-stock"
-                        icon="plus-circle" class="text-amber-700!">
-                        Ingreso Stock
-                    </flux:button>
-
-                    <flux:button href="/admin/movimientos/salida/{{ $producto->id }}/create-venta"
-                        icon="banknotes" class="text-green-700!">
-                        Venta Rápida
-                    </flux:button>
-
-                    @can('update', App\Models\Producto::class)
-                        <!-- Botón editar -->
-                        <flux:button href="{{ route('productos.edit', $producto->id) }}" icon="pencil-square"
-                            class="text-blue-700!">
-                            Editar
-                        </flux:button>
-                    @endcan
-                    @can('delete', App\Models\Producto::class)
-                        <flux:button data-bs-toggle="modal" data-bs-target="#confirmDelete{{ $producto->id }}" icon="trash"
-                            class="text-red-500!">
-                            Eliminar
-                        </flux:button>
-                    @endcan
-
-                    <!-- Modal de confirmación de eliminación -->
-                    <div class="modal fade fixed top-0 left-0 w-full h-full bg-black/50 z-50 hidden"
-                        id="confirmDelete{{ $producto->id }}" tabindex="-1" aria-hidden="true">
-                        <div class="modal-dialog relative top-1/4 mx-auto max-w-md">
-                            <div class="modal-content bg-white rounded-lg shadow-lg p-6">
-                                <div class="modal-header flex justify-between items-center">
-                                    <h5 class="text-xl font-medium">Confirmar eliminación</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                                </div>
-                                <div class="modal-body mt-4">
-                                    <p>¿Estás seguro de que deseas eliminar
-                                        <strong>{{ $producto->nombre_producto }}</strong>?
-                                    </p>
-                                </div>
-                                <div class="modal-footer flex justify-end mt-4 space-x-2">
-                                    <form action="{{ route('productos.destroy', $producto->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit"
-                                            class="bg-red-600 hover:bg-red-700 text-white font-semibold py-1.5 px-4 rounded text-sm">
-                                            Sí, eliminar</button>
-                                    </form>
-                                    <button type="button"
-                                        class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-1.5 px-4 rounded text-sm"
-                                        data-bs-dismiss="modal">Cancelar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Modal para ver detalles de producto (con bootstrap)-->
-                    <!-- Se activará cuando se clickee el nombre del producto -->
-                    <!-- Modal -->
-                    <div class="modal fade fixed inset-0 z-50 bg-black/50 hidden" id="modal{{ $producto->id }}"
-                        tabindex="-1" aria-hidden="true">
-
-                        <div class="modal-dialog relative mx-auto mt-24 w-full max-w-sm">
-                            <div class="modal-content bg-white rounded-lg shadow-lg max-h-[80vh] overflow-y-auto">
-
-                                <!-- Header -->
-                                <div class="modal-header flex items-center justify-between p-4 border-b">
-                                    <h5 class="text-lg font-semibold text-gray-900">{{ $producto->nombre_producto }}</h5>
-                                    <button type="button"
-                                        class="text-gray-500 hover:text-gray-700 focus:outline-none text-2xl font-bold"
-                                        data-bs-dismiss="modal" aria-label="Close">
-                                        &times;
-                                    </button>
-                                </div>
-
-                                <!-- Body -->
-                                <div class="modal-body p-4 space-y-3 text-gray-800">
-                                    <img src="https://placehold.co/500x500" alt="Imagen del producto"
-                                        class="aspect-square w-full bg-gray-200 object-cover rounded">
-
-                                    <p><strong>Precio:</strong> ${{ number_format($producto->precio, 0, ',', '.') }}</p>
-                                    <p><strong>Descripción:</strong> {{ $producto->descripcion }}</p>
-                                </div>
-
-                                <!-- Footer -->
-                                <div class="modal-footer flex justify-end p-4 border-t">
-                                    <button type="button"
-                                        class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded"
-                                        data-bs-dismiss="modal">
-                                        Cerrar
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            @endforeach
+                                    @can('update', App\Models\Producto::class)
+                                        <flux:button href="{{ route('productos.edit', $producto->id) }}" icon="pencil-square"
+                                            class="text-blue-700!" tooltip="Editar Datos" />
+                                    @endcan
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     @else
         <div class="flex items-center justify-center h-64">
